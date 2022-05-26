76066debb99c4f4cce75fd7960d6c3601396e93b
Nick Lototskiy <nicklototskiy@n-dev-05505.inner.directory>
2022-05-26 12:04:51 +0200

    feat: add bulk upload for employees

--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -3,6 +3,8 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { EmployeeEntity } from "../model/employee.entity";
 import {InsertResult, Repository} from "typeorm";
 import {ApiOperation} from "@nestjs/swagger";
+import {readFileSync} from "fs";
+import { parse }  from 'papaparse';
 
 @Injectable()
 export class EmployeeService {
@@ -21,7 +23,31 @@ export class EmployeeService {
 
     @ApiOperation({ summary: 'Get by ID' })
     public async findById(id: number): Promise<EmployeeEntity[]>  {
-        console.log(id)
         return await this.repo.findByIds([id]);
+    }
+
+    public async createFromFile(fileName:string) {
+        const csvFile = readFileSync(`./uploads/${fileName}`);
+        const csvData = csvFile.toString();
+
+        const parsedCSV = await parse(csvData, {
+            header: true,
+            skipEmptyLines: true,
+            transformHeader: (header) => header.toLowerCase().replace('#', '').trim(),
+            complete: (results) => results.data,
+        });
+
+        if(parsedCSV.data && parsedCSV.data.length != 0) {
+            parsedCSV.data.forEach((item) => {
+                    const employee = new EmployeeEntity();
+                    employee.name = item['name'];
+                    employee.second_name = item['second_name'];
+                    employee.description = item['description'];
+                    employee.organization_id = item['organization_id'];
+                    employee.wallet_id = item['wallet_id'];
+                    employee.amount = item['amount'];
+                    this.repo.insert(employee);
+            });
+        }
     }
 }
