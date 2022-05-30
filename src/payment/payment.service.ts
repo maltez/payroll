89bc8f6aa2ab953b26299384880258e5af430854
Artem Nuss <artem.nuss@megadevllc.com>
2022-05-30 20:40:35 +0300

    refactoring

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -9,6 +9,9 @@ const contract_abi = require('../../ABI/abi.json');
 
 const contract_address = '0x087291774ee2d2dbdf19caa804ad818d78123f10';
 const web3 = new Web3('https://godwoken-testnet-v1.ckbapp.dev'); // Your Web3 instance
+
+const colleteralToken = new web3.eth.Contract(contract_abi, contract_address);
+
 @Injectable()
 export class PaymentService {
     constructor(@InjectRepository(PaymentEntity) private readonly repo: Repository<PaymentEntity>) {
@@ -22,9 +25,7 @@ export class PaymentService {
     @ApiOperation({summary: 'Get  balance'})
     public async getBalance(id: string) {
 
-        const NameContract = new web3.eth.Contract(contract_abi, contract_address);
-
-        const balance = await NameContract.methods.balanceOf(id).call();
+        const balance = await colleteralToken.methods.balanceOf(id).call();
 
         console.log(balance)
     }
@@ -32,8 +33,7 @@ export class PaymentService {
     @ApiOperation({summary: 'Mint'})
     public async mint(@Body() address: string): Promise<InsertResult> {
         const to = address['address']
-        const NameContract = new web3.eth.Contract(contract_abi, contract_address);
-        let trans = NameContract.methods.mint(to, web3.utils.toWei('100', 'ether')).encodeABI();
+        const trans = colleteralToken.methods.mint(to, web3.utils.toWei('100', 'ether')).encodeABI();
         const createTransaction = await web3.eth.accounts.signTransaction(
             {
                 to: contract_address,
@@ -45,39 +45,27 @@ export class PaymentService {
             '0x97d2e58fbb30d9a004683eef68f8ec26cacb19e39bd4331a2508ab5c36e70b0b'
         );
 
-        const balance = await NameContract.methods.balanceOf(contract_address).call();
+        const balance = await colleteralToken.methods.balanceOf(contract_address).call();
 
-        const tx = await web3.eth.sendSignedTransaction(createTransaction.rawTransaction)
-
-        console.log(tx)
+        await web3.eth.sendSignedTransaction(createTransaction.rawTransaction)
         return balance
     }
 
     @ApiOperation({summary: 'Create payment'})
     public async create(@Body() create: PaymentEntity): Promise<InsertResult> {
-        const deploy = async () => {
-            const NameContract = new web3.eth.Contract(contract_abi, contract_address);
+        const trans = colleteralToken.methods.transfer(create.wallet_id, web3.utils.toWei(`${create.value}`, 'ether')).encodeABI();
+        const createTransaction = await web3.eth.accounts.signTransaction(
+            {
+                to: contract_address,
+                value: 0,
+                gas: '210000',
+                data: trans,
+                gasPrice: 10 ** 9
+            },
+            create.payer_secret_id
+        );
 
-            const trans = NameContract.methods.transfer(create.wallet_id, web3.utils.toWei(`${create.value}`, 'ether')).encodeABI();
-            const createTransaction = await web3.eth.accounts.signTransaction(
-                {
-                    to: contract_address,
-                    value: 0,
-                    gas: '210000',
-                    data: trans,
-                    gasPrice: 10 ** 9
-                },
-                create.payer_secret_id
-            );
-
-            const createReceipt = await web3.eth.sendSignedTransaction(
-                createTransaction.rawTransaction
-            );
-
-            return createReceipt;
-        };
-
-        deploy();
+        await web3.eth.sendSignedTransaction(createTransaction.rawTransaction);
         return this.repo.insert(create);
     }
 
